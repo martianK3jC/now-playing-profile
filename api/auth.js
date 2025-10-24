923bf6f6325a0780822ce67a8a04c322cc1df317
K_shang <kceniza24@gmail.com>
2025-10-24 22:20:00 +0800

    Update redirect URI to use production URL placeholder

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res) => {
   const scope = 'user-read-currently-playing user-read-recently-played';
-  const redirect_uri = 'https://now-playing-profile-qy3ol6wtf-martiank3jcs-projects.vercel.app/api/callback';
+  // Use your production URL here
+  const redirect_uri = 'https://YOUR_PRODUCTION_URL.vercel.app/api/callback';
   
   const authUrl = 'https://accounts.spotify.com/authorize?' +
     new URLSearchParams({
